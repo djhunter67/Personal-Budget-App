9e1c2f8ce19b3b96155ea12e18586edfa0d35446
Christerpher Hunter <djhunter67@gmail.com>
2021-07-06 11:13:13 -0400

    runs from __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,20 @@
-from budget import Budget, Budget_Data
+from budget_gui import Budget_GUI
+import tkinter as tk
 
 
-if __name__ ==   '__main__':
-    pass
+def main():
+
+    root = tk.Tk()
+    root.title("BUDGET GUI")
+    w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+    root.geometry(f"{w // 14}x{h // 8}+0+0")
+    root.focus_set()
+    root.bind("<Escape>", lambda e: e.widget.quit())
+
+    Budget_GUI(root)
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
